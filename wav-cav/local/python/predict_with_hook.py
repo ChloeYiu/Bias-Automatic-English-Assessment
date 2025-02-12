ebb3d247ebb53bb40bacb1bd7b7f6e42097459de
ChloeYiu <chloe.yiu.yee.man@gmail.com>
2025-02-12 20:27:47 +0000

    predict using model to generate activation and gradient

--- wav-cav/local/python/predict_with_hook.py
+++ wav-cav/local/python/predict_with_hook.py
@@ -1,14 +1,128 @@
+import math
+import torch
+import numpy as np
+import argparse
+import os
+from transformers import AutoConfig, Wav2Vec2Processor
 from datasets import load_from_disk
+from train import Wav2Vec2ForSpeechClassification
+from cav import ActivationGetter
+
+device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
+print(f"Device: {device}")
+print(f"Device name: {torch.cuda.get_device_name(1)}")
+
+def calc_feat_size(audio_len):
+    '''
+    The Wav2Vec2 base model uses 25 ms frames and 20 ms steps.
+    This code assumes that the audio sampling frequency is 16 kHz.
+    '''
+    assert type(audio_len) == int # check that function input is an integer
+    if audio_len < 400:
+        return "The length of the audio segment must be at least 25 ms or 400 samples."
+    else:
+        n_feat_vecs = 1 + math.floor(round((audio_len-400)/320, 5)) # the rounding is precautionary for quantization error
+
+    return n_feat_vecs
+
+def speech_file_to_array_fn(batch):
+    arrays = []
+    for speech_array in batch["input_values"]:
+        arrays.append(speech_array)
+
+    batch["speech"] = arrays
+    return batch
+
+def predict(batch, model, device, activation_getter, processor):
+    all_feats = None
+    list_len = [len(i) for i in batch["speech"]]
+    pad_to = max(list_len)
+    paded = [np.pad(test, (0, pad_to-len(test)), 'constant', constant_values=(0, 0)) for test in batch["speech"]]
+
+    audio_lengths = []
+    for test in batch["speech"]:
+        for seg in test:
+            audio_lengths.append(min(480000, len(seg))) # capping at 240,000 is probably superfluous, but is precautionary to ensure no audios longer than 15 s
+    max_feat_vecs = calc_feat_size(max(audio_lengths))
+    attention_mask = np.zeros((len(paded), pad_to, max_feat_vecs)) # setting attention_heads_mask shape as 4D tensor (# tests in batch, max # segs in test, max # feat_vecs in seg)
+    for ind_test, test in enumerate(batch["speech"]):
+        for ind_seg, seg in enumerate(test):
+            attention_mask[ind_test, ind_seg, :calc_feat_size(len(batch["speech"][ind_test][ind_seg]))] = np.ones((calc_feat_size(len(batch["speech"][ind_test][ind_seg]))))
+
+    paded = [[np.zeros(max(audio_lengths)) if type(aud) == np.int64 else aud for aud in test] for test in paded]
+    paded = [[np.pad(aud, (0, max(audio_lengths)-len(aud))) if len(aud) < max(audio_lengths) else aud for aud in test] for test in paded]
+    paded = [[aud[:480000] if len(aud)>480000 else aud for aud in feature] for feature in paded]
+
+    for test in paded:
+        features = torch.unsqueeze(processor(test, sampling_rate=processor.feature_extractor.sampling_rate, return_tensors="pt", padding=True)["input_values"],0)
+        if all_feats == None:
+            all_feats = features
+        else:
+            all_feats = torch.cat((all_feats, features), dim=0)
+    input_values = all_feats.to(device)
+    attention_mask = torch.tensor(attention_mask, dtype=torch.bool)
+    attention_heads_mask = attention_mask.to(device)
+    
+    logits = model(input_values, attention_heads_mask).logits 
+
+    for layer_name in activation_getter.layer_names:
+        activations = activation_getter.activation_cache[layer_name]
+        # Compute gradients for this sample
+        grad = torch.autograd.grad(
+            outputs=logits,
+            inputs=activations,
+            grad_outputs=torch.ones_like(logits),
+            create_graph=False,
+            retain_graph=False,
+            allow_unused=True
+        )
+        activation_getter.store_tmp_gradient(grad)
+
+    batch["predicted"] = logits.detach().cpu().numpy()
+    return batch
+
 
 def main(args):
-    test_data = args.TEST_DATA #'/scratches/dialfs/alta/sb2549/wav2vec2_exp/data_vectors_attention/LIESTcal01/LIESTcal01_part4_att.hf/'
+    test_data = args.DATA_DIR #'/scratches/dialfs/alta/sb2549/wav2vec2_exp/data_vectors_attention/LIESTcal01/LIESTcal01_part4_att.hf'
+    model_dir = args.MODEL_DIR 
+    activation_dir = args.ACTIVATION_DIR
+    gradient_dir = args.GRADIENT_DIR
+    output_file = args.OUTPUT_FILE
+    output_dir = os.path.dirname(output_file)
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    config = AutoConfig.from_pretrained(model_dir)
+    processor = Wav2Vec2Processor.from_pretrained("patrickvonplaten/wav2vec2-base")
+    model = Wav2Vec2ForSpeechClassification.from_pretrained(model_dir).to(device)
+
+    model_part, model_seed = model_dir.split('/')[1], model_dir.split('/')[2]
+    model_name = f"{model_part}_{model_seed}"
+    activation_getter = ActivationGetter(model.classifier, model_name, activation_dir, gradient_dir, ['dense'], 1)
     test_dataset = load_from_disk(test_data)
     test_dataset = test_dataset.map(speech_file_to_array_fn)
-    test_ds = test_dataset.map(predict, batched=True, batch_size=32)                                                                                                                  
+    #test_dataset.save_to_disk()
+
+    activation_getter.add_hooks()
+    test_ds = test_dataset.map(lambda batch: predict(batch, model, device, activation_getter, processor), batched=True, batch_size=1)
+    activation_getter.remove_hooks()
+
+    speaker_id, ref_score, pred_score = test_ds['base_id'], test_ds['labels'], test_ds['predicted']
+    with open(output_file, 'w') as f:
+        f.write('SPEAKERID REF PRED\n')
+        for spkr, ref, pred in zip(speaker_id, ref_score, pred_score):
+            f.write(f'{spkr} {pred} {ref}\n')
+
+    # Save the activations for each layer
+    activation_getter.store_activations(speaker_id)
+    activation_getter.store_gradients(speaker_id)
 
 if __name__ == '__main__':
     commandLineParser = argparse.ArgumentParser()
-    commandLineParser.add_argument('--TEST_DATA', type=str, help='file with CAV')
+    commandLineParser.add_argument('--DATA_DIR', type=str, help='directory with test data')
+    commandLineParser.add_argument('--MODEL_DIR', type=str, help='directory with model')
+    commandLineParser.add_argument('--ACTIVATION_DIR', type=str, help='directory to store activations')
+    commandLineParser.add_argument('--GRADIENT_DIR', type=str, help='directory to store gradients')
     commandLineParser.add_argument('--OUTPUT_FILE', type=str, help='file to save prediction')
     args = commandLineParser.parse_args()
     main(args)
--- wav-cav/local/run/predict_with_hook.sh
+++ wav-cav/local/run/predict_with_hook.sh
@@ -4,10 +4,12 @@
 ALLARGS="$0 $@"
 ARGS="$@"
 
-export PATH="/scratches/dialfs/alta/hkv21/mconda_setup/miniconda_py3.11/envs/pytorch2.0/bin:$PATH"
-condaenv=/scratches/dialfs/alta/hkv21/mconda_setup/miniconda_py3.11/envs/pytorch2.0/
-source activate "$condaenv"
+export PATH="/scratches/dialfs/alta/oet/exp-swm35/housekeeping/envs/pyannote_w2v2/bin:$PATH"
+condaenv=/scratches/dialfs/alta/oet/exp-swm35/housekeeping/envs/pyannote_w2v2/
+source /scratches/dialfs/alta/oet/exp-swm35/housekeeping/etc/profile.d/conda.sh
 
+conda activate "$condaenv"
+echo "Activated environment: $(conda info --envs | grep '*' | awk '{print $1}')"
 which python
 python --version
 
@@ -20,20 +22,25 @@ fi
 train_set=$1
 test_set=$2
 
-LOG=LOGs/run/run_prediction.log
+
+declare -a seeds=(24)
+
+for part in 1; do
+  for seed in "${seeds[@]}"; do
+    
+    top_outdir=eval/$train_set/part$part
+    model_dir=models/$train_set/part$part/seed$seed/checkpoint-1968
+    output_file=$top_outdir/predictions/$test_set/preds_wav2vec_part${part}_seed${seed}
+
+    LOG=LOGs/$output_file.log
     mkdir -p $(dirname $LOG)
     if [ -f $LOG ]; then
         \rm $LOG
     fi
 
-declare -a seeds=(10 30 50 70 90)
+    echo "Logging to $LOG"
 
-for part in 1; do
-  for seed in "${seeds[@]}"; do
-    
-    top_outdir=eval/$train_set/part$part/
-
-    cmd="python local/python/predict_with_hook.py --data_dir /scratches/dialfs/alta/sb2549/wav2vec2_exp/data_vectors_attention/$test_set/${test_set}_part${part}_att.hf --model_dir $top_outdir/models --GRADIENT_DIR $top_outdir/gradients/$test_set --ACTIVATION_DIR $top_outdir/activations/$test_set"
+    cmd="python local/python/predict_with_hook.py --DATA_DIR data_vectors_attention/$test_set/${test_set}_part${part}_att.hf --MODEL_DIR $model_dir --GRADIENT_DIR $top_outdir/gradients/$test_set --ACTIVATION_DIR $top_outdir/activations/$test_set --OUTPUT_FILE $output_file.txt"
 
     echo $cmd
     $cmd >> $LOG 2>&1
