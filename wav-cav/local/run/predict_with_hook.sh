df0ed8ec7e1422652fa178fc360ebd1905bbc628
ChloeYiu <chloe.yiu.yee.man@gmail.com>
2025-02-17 17:21:28 +0000

    update predict with hook for biased prediction

--- wav-cav/local/run/predict_with_hook.sh
+++ wav-cav/local/run/predict_with_hook.sh
@@ -13,24 +13,57 @@ echo "Activated environment: $(conda info --envs | grep '*' | awk '{print $1}')"
 which python
 python --version
 
+while [ $# -gt 0 ]; do
+    key=$1
+    case $key in
+        --biased_train)
+        cmdopts="$cmdopts $1 $2"
+        biased_train=$2
+        shift
+        shift
+        ;;
+        --biased_test)
+        cmdopts="$cmdopts $1 $2"
+        biased_test=$2
+        shift
+        shift
+        ;;
+    *)
+    POSITIONAL+=("$1")
+    shift
+    ;;
+    esac
+done
+set -- "${POSITIONAL[@]}"
 
 if [ $# -lt 2 ]; then
-  echo "Usage: $0 <train_set> <test_set>"
+  echo "Usage: $0 <train_set> <test_set> --biased_train <biased_train> --biased_test <biased_test>"
   exit 1
 fi
 
 train_set=$1
 test_set=$2
 
-
 declare -a seeds=(24)
 
 for part in 1; do
   for seed in "${seeds[@]}"; do
     
-    top_outdir=eval/$train_set/part$part
-    model_dir=models/$train_set/part$part/seed$seed/checkpoint-1968
-    output_file=$top_outdir/predictions/$test_set/preds_wav2vec_part${part}_seed${seed}
+    if [ -z "$biased_train" ]; then
+      top_outdir=eval/$train_set/part$part
+      model_dir=models/$train_set/part$part/seed$seed/checkpoint-1968
+    else
+      top_outdir=eval/${train_set}_${biased_train}/part$part
+      model_dir=models/${train_set}_${biased_train}/part$part/seed$seed/checkpoint-1968
+    fi
+
+    if [ -z "$biased_test" ]; then
+      output_file=$top_outdir/predictions/$test_set/preds_wav2vec_part${part}_seed${seed}
+      biased_score=None
+    else
+      output_file=$top_outdir/predictions/${test_set}_${biased_test}/preds_wav2vec_part${part}_seed${seed}
+      biased_score=models/${test_set}_${biased_test}/part$part/scores_biased.json
+    fi
 
     LOG=LOGs/$output_file.log
     mkdir -p $(dirname $LOG)
@@ -40,7 +73,7 @@ for part in 1; do
 
     echo "Logging to $LOG"
 
-    cmd="python local/python/predict_with_hook.py --DATA_DIR data_vectors_attention/$test_set/${test_set}_part${part}_att.hf --MODEL_DIR $model_dir --GRADIENT_DIR $top_outdir/gradients/$test_set --ACTIVATION_DIR $top_outdir/activations/$test_set --OUTPUT_FILE $output_file.txt"
+    cmd="python local/python/predict_with_hook.py --DATA_DIR data_vectors_attention/$test_set/${test_set}_part${part}_att.hf --MODEL_DIR $model_dir --GRADIENT_DIR $top_outdir/gradients/$test_set --ACTIVATION_DIR $top_outdir/activations/$test_set --OUTPUT_FILE $output_file.txt --BIASED_SCORE $biased_score"
 
     echo $cmd
     $cmd >> $LOG 2>&1
