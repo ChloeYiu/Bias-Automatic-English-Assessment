99f91e545b4e4a95f0adca2a47655825000051b3
ChloeYiu <chloe.yiu.yee.man@gmail.com>
2025-02-18 00:16:52 +0000

    update how eval_model takes input

--- wav-cav/local/run/eval_model.sh
+++ wav-cav/local/run/eval_model.sh
@@ -14,36 +14,33 @@ echo "Activated environment: $(conda info --envs | grep '*' | awk '{print $1}')"
 which python
 python --version
 
-if [ $# -lt 2 ]; then
-  echo "Usage: $0 <trainset> <testset>"
+if [ $# -lt 3 ]; then
+  echo "Usage: $0 <trainset> <testset> <file_suffix>"
   exit 1
 fi
 
 trainset=$1
 testset=$2
-
-declare -a seeds=(2 24)
+file_suffix=$3
 
 for part in 1; do
   top_outdir=eval/$trainset/part$part
 
-  for seed in "${seeds[@]}"; do
-    prediction_file=$top_outdir/predictions/$testset/preds_wav2vec_part${part}_seed${seed}.txt
-    output_base_name=$top_outdir/predictions/$testset/eval_wav2vec_part${part}_seed${seed}
+  prediction_file=$top_outdir/predictions/$testset/preds_wav2vec_part${part}_${file_suffix}.txt
+  output_base_name=$top_outdir/predictions/$testset/eval_wav2vec_part${part}_${file_suffix}
 
-    log_file="LOGs/$output_base_name.log"
-    echo "Log file: $log_file"
+  log_file="LOGs/$output_base_name.log"
+  echo "Log file: $log_file"
 
-    # Create log directory if it does not exist
-    mkdir -p $(dirname "$log_file")
-    if [ -f "$log_file" ]; then
-        rm "$log_file"
-    fi
+  # Create log directory if it does not exist
+  mkdir -p $(dirname "$log_file")
+  if [ -f "$log_file" ]; then
+      rm "$log_file"
+  fi
 
-    # Run the evaluation script with arguments from JSON file
-    cmd="python local/python/eval_model.py --PREDICTION_FILE $prediction_file --OUTPUT_FILE $output_base_name.txt"
-    echo $cmd
-    $cmd >> $log_file 2>&1
+  # Run the evaluation script with arguments from JSON file
+  cmd="python local/python/eval_model.py --PREDICTION_FILE $prediction_file --OUTPUT_FILE $output_base_name.txt"
+  echo $cmd
+  $cmd >> $log_file 2>&1
 
-  done
 done
