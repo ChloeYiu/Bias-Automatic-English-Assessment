697817c0cd9cdace9b169748b0a39606fb9ced2c
ChloeYiu <chloe.yiu.yee.man@gmail.com>
2025-02-25 17:23:07 +0000

    batch run post train scripts

---
+++ wav-cav/batch/batch_posttrain.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#$ -S /bin/bash
+
+ALLARGS="$0 $@"
+ARGS="$@"
+
+profile=$1
+
+local/run/predict_with_hook.sh LIESTgrp06 LIESTgrp06 --biased_train $profile --biased_test $profile
+local/run/predict_with_hook.sh LIESTgrp06 LIESTdev02 --biased_train $profile
+local/run/extract_cav.sh LIESTgrp06_$profile LIESTgrp06_$profile $profile
+local/run/extract_cav.sh LIESTgrp06_$profile LIESTgrp06_$profile $profile --class_weight balanced
+local/run/eval_bias_multiple.sh LIESTgrp06_$profile LIESTgrp06_$profile LIESTdev02 $profile
+local/run/eval_bias_multiple.sh LIESTgrp06_$profile LIESTgrp06_$profile LIESTdev02 $profile --class_weight balanced
+local/run/compare.sh LIESTgrp06 LIESTdev02 $profile $profile
+local/run/compare.sh LIESTgrp06 LIESTdev02 $profile $profile --class_weight balanced
