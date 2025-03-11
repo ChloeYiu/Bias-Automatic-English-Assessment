e10798aa2fbf6b43d2d832391feaf2d105258f31
ChloeYiu <chloe.yiu.yee.man@gmail.com>
2025-03-11 16:18:16 +0000

    remove commented cmd for batch eval bias multiple

--- feature-cav/batch/batch_eval_bias_multiple.sh
+++ feature-cav/batch/batch_eval_bias_multiple.sh
@@ -13,10 +13,10 @@ run/eval_bias_multiple.sh LIESTgrp06 LIESTgrp06 LIESTdev02 $model thai
 run/eval_bias_multiple.sh LIESTgrp06 LIESTgrp06 LIESTdev02 $model spanish
 run/eval_bias_multiple.sh LIESTgrp06 LIESTgrp06 LIESTdev02 $model young
 run/eval_bias_multiple.sh LIESTgrp06 LIESTgrp06 LIESTdev02 $model male
-# run/eval_bias_multiple.sh LIESTgrp06 LIESTgrp06 LIESTdev02 $model grade_A --class_weight balanced
-# run/eval_bias_multiple.sh LIESTgrp06 LIESTgrp06 LIESTdev02 $model grade_B2 --class_weight balanced
-# run/eval_bias_multiple.sh LIESTgrp06 LIESTgrp06 LIESTdev02 $model grade_C --class_weight balanced
-# run/eval_bias_multiple.sh LIESTgrp06 LIESTgrp06 LIESTdev02 $model thai --class_weight balanced
-# run/eval_bias_multiple.sh LIESTgrp06 LIESTgrp06 LIESTdev02 $model spanish --class_weight balanced
-# run/eval_bias_multiple.sh LIESTgrp06 LIESTgrp06 LIESTdev02 $model young --class_weight balanced
-# run/eval_bias_multiple.sh LIESTgrp06 LIESTgrp06 LIESTdev02 $model male --class_weight balanced
+run/eval_bias_multiple.sh LIESTgrp06 LIESTgrp06 LIESTdev02 $model grade_A --class_weight balanced
+run/eval_bias_multiple.sh LIESTgrp06 LIESTgrp06 LIESTdev02 $model grade_B2 --class_weight balanced
+run/eval_bias_multiple.sh LIESTgrp06 LIESTgrp06 LIESTdev02 $model grade_C --class_weight balanced
+run/eval_bias_multiple.sh LIESTgrp06 LIESTgrp06 LIESTdev02 $model thai --class_weight balanced
+run/eval_bias_multiple.sh LIESTgrp06 LIESTgrp06 LIESTdev02 $model spanish --class_weight balanced
+run/eval_bias_multiple.sh LIESTgrp06 LIESTgrp06 LIESTdev02 $model young --class_weight balanced
+run/eval_bias_multiple.sh LIESTgrp06 LIESTgrp06 LIESTdev02 $model male --class_weight balanced
