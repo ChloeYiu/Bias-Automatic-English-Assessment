ed3685f91bbce59cf2aa0a107bc7fc3ad0de7ea3
ChloeYiu <chloe.yiu.yee.man@gmail.com>
2025-03-11 14:53:43 +0000

    update batch cav extract

--- feature-cav/batch/batch_extract_cav.sh
+++ feature-cav/batch/batch_extract_cav.sh
@@ -1,14 +1,22 @@
-run/extract_cav.sh LIESTgrp06 LIESTgrp06 grade_A
-run/extract_cav.sh LIESTgrp06 LIESTgrp06 grade_B2
-run/extract_cav.sh LIESTgrp06 LIESTgrp06 grade_C
-run/extract_cav.sh LIESTgrp06 LIESTgrp06 thai
-run/extract_cav.sh LIESTgrp06 LIESTgrp06 spanish
-run/extract_cav.sh LIESTgrp06 LIESTgrp06 young
-run/extract_cav.sh LIESTgrp06 LIESTgrp06 male
-run/extract_cav.sh LIESTgrp06 LIESTgrp06 grade_A --class_weight balanced
-run/extract_cav.sh LIESTgrp06 LIESTgrp06 grade_B2 --class_weight balanced
-run/extract_cav.sh LIESTgrp06 LIESTgrp06 grade_C --class_weight balanced
-run/extract_cav.sh LIESTgrp06 LIESTgrp06 thai --class_weight balanced
-run/extract_cav.sh LIESTgrp06 LIESTgrp06 spanish --class_weight balanced
-run/extract_cav.sh LIESTgrp06 LIESTgrp06 young --class_weight balanced
-run/extract_cav.sh LIESTgrp06 LIESTgrp06 male --class_weight balanced
+#!/bin/bash
+#$ -S /bin/bash
+
+ALLARGS="$0 $@"
+ARGS="$@"
+
+model=$1
+
+run/extract_cav.sh LIESTgrp06 LIESTgrp06 grade_A $model
+run/extract_cav.sh LIESTgrp06 LIESTgrp06 grade_B2 $model
+run/extract_cav.sh LIESTgrp06 LIESTgrp06 grade_C $model
+run/extract_cav.sh LIESTgrp06 LIESTgrp06 thai $model
+run/extract_cav.sh LIESTgrp06 LIESTgrp06 spanish $model
+run/extract_cav.sh LIESTgrp06 LIESTgrp06 young $model
+run/extract_cav.sh LIESTgrp06 LIESTgrp06 male $model
+run/extract_cav.sh LIESTgrp06 LIESTgrp06 grade_A $model --class_weight balanced
+run/extract_cav.sh LIESTgrp06 LIESTgrp06 grade_B2 $model --class_weight balanced
+run/extract_cav.sh LIESTgrp06 LIESTgrp06 grade_C $model --class_weight balanced
+run/extract_cav.sh LIESTgrp06 LIESTgrp06 thai $model --class_weight balanced
+run/extract_cav.sh LIESTgrp06 LIESTgrp06 spanish $model --class_weight balanced
+run/extract_cav.sh LIESTgrp06 LIESTgrp06 young $model --class_weight balanced
+run/extract_cav.sh LIESTgrp06 LIESTgrp06 male $model --class_weight balanced
